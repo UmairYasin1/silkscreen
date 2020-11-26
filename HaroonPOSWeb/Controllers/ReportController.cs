c64293253516ad38a80c6b3c970569ac01f2720f
Umair Yasin <umair_yasin@hotmail.com>
2020-11-26 09:41:23 +0500

    commit with stock report

--- HaroonPOSWeb/Controllers/ReportController.cs
+++ HaroonPOSWeb/Controllers/ReportController.cs
@@ -38,7 +38,12 @@ namespace HaroonPOSWeb.Controllers
             return View();
         }
 
-
+        public ActionResult StockReport()
+        {
+            GetMonthList();
+            GetYearList();
+            return View();
+        }
 
         #region download Party Purchase Ledger pdf
 
@@ -358,6 +363,168 @@ namespace HaroonPOSWeb.Controllers
 
         // Method to add single cell to the body  
         private static void AddCellToBody_PartySale(PdfPTable tableLayout, string cellText)
+        {
+            tableLayout.AddCell(new PdfPCell(new Phrase(cellText, new iTextSharp.text.Font(iTextSharp.text.Font.FontFamily.HELVETICA, 8, 1, iTextSharp.text.BaseColor.BLACK)))
+            {
+                HorizontalAlignment = Element.ALIGN_LEFT,
+                Padding = 5,
+                BackgroundColor = new iTextSharp.text.BaseColor(255, 255, 255)
+            });
+        }
+
+        #endregion
+
+
+        #region download Party Sale Ledger pdf
+
+
+        public FileResult CreatePdfStockReport(string stringValues)
+        {
+
+            string[] values = stringValues.Split('?');
+            for (int i = 0; i < values.Length; i++)
+            {
+                values[i] = values[i].Trim();
+            }
+
+            //var MatOutPartyId = values[0];
+            var MonthId = values[1].Replace("MonthId=", "");
+            var YearId = values[2].Replace("YearId=", "");
+
+            //int MatOutPartyIdVal = Convert.ToInt32(MatOutPartyId);
+            int MonthIdVal = Convert.ToInt32(MonthId);
+            int YearIdVal = Convert.ToInt32(YearId);
+
+            MemoryStream workStream = new MemoryStream();
+            StringBuilder status = new StringBuilder("");
+            DateTime dTime = DateTime.Now;
+            //file name to be created   
+            string strPDFFileName = string.Format("StockReport.pdf");
+            Document doc = new Document();
+            doc.SetMargins(0f, 0f, 0f, 0f);
+            //Create PDF Table with 5 columns  
+            PdfPTable tableLayout = new PdfPTable(6);
+            //doc.SetMargins(0f, 0f, 0f, 0f);
+            //Create PDF Table  
+
+            string strAttachment = Server.MapPath("~/" + strPDFFileName);
+
+
+            PdfWriter.GetInstance(doc, workStream).CloseStream = false;
+            doc.Open();
+
+            //Add Content to PDF   
+            doc.Add(Add_Content_To_PDF_StockReport(tableLayout, MonthIdVal, YearIdVal));
+
+            // Closing the document  
+            doc.Close();
+
+            byte[] byteInfo = workStream.ToArray();
+            workStream.Write(byteInfo, 0, byteInfo.Length);
+            workStream.Position = 0;
+
+            return File(workStream, "application/pdf", strPDFFileName);
+
+        }
+
+        protected PdfPTable Add_Content_To_PDF_StockReport(PdfPTable tableLayout, int MonthId, int YearId)
+        {
+
+            float[] headers = { 25, 50, 50, 25, 25, 50 }; //Header Widths  
+            tableLayout.SetWidths(headers); //Set the pdf headers  
+            tableLayout.WidthPercentage = 100; //Set the PDF File witdh percentage  
+            tableLayout.HeaderRows = 1;
+            //Add Title to the PDF file at the top  
+
+
+            var month = db.Months.Where(x => x.MonthId.Equals(MonthId)).FirstOrDefault();
+            var year = db.Years.Where(x => x.YearId.Equals(YearId)).FirstOrDefault();
+            int monthVal = MonthId;
+            int yearVal = Convert.ToInt32(year.YearName);
+            var stockList = db.StockDatas.Where(x => x.CreateDate.Value.Month.Equals(monthVal)
+                                    && x.CreateDate.Value.Year.Equals(yearVal)).ToList();
+
+            tableLayout.AddCell(new PdfPCell(new Phrase("Stock report according to " + month.MonthName + "-" + year.YearName,
+                    new iTextSharp.text.Font(iTextSharp.text.Font.FontFamily.HELVETICA, 8, 1, new iTextSharp.text.BaseColor(0, 0, 0))))
+            {
+                Colspan = 12,
+                Border = 0,
+                PaddingBottom = 5,
+                HorizontalAlignment = Element.ALIGN_CENTER
+            });
+
+
+            ////Add header  
+            AddCellToHeader_StockReport(tableLayout, "S#");
+            AddCellToHeader_StockReport(tableLayout, "Item");
+            AddCellToHeader_StockReport(tableLayout, "Date");
+            AddCellToHeader_StockReport(tableLayout, "Quantity");
+            AddCellToHeader_StockReport(tableLayout, "Est. Rate");
+            AddCellToHeader_StockReport(tableLayout, "Sub Amount");
+
+            ////Add body  
+
+            if (stockList != null && stockList.Count != 0)
+            {
+                int serialNo = 0;
+                int grandTotal = 0;
+                var last = stockList.Last();
+                foreach (var tS in stockList)
+                {
+                    serialNo += 1;
+                    var quantityVal = tS.Quantity;
+                    var rateVal = tS.Rate;
+                    int subAmount = Convert.ToInt32(quantityVal * rateVal);
+                    grandTotal += subAmount;
+
+                    AddCellToBody_PartySale(tableLayout, serialNo.ToString());
+                    AddCellToBody_PartySale(tableLayout, tS.ItemName);
+                    AddCellToBody_PartySale(tableLayout, tS.Date.Value.ToString("dd-MM-yyyy"));
+                    AddCellToBody_PartySale(tableLayout, tS.Quantity.Value.ToString("."));
+                    AddCellToBody_PartySale(tableLayout, tS.Rate.Value.ToString("."));
+                    AddCellToBody_PartySale(tableLayout, subAmount.ToString("."));
+
+                    if (tS.Equals(last))
+                    {
+                        AddCellToBody_StockReport(tableLayout, "");
+                        AddCellToBody_StockReport(tableLayout, "");
+                        AddCellToBody_StockReport(tableLayout, "");
+                        AddCellToBody_StockReport(tableLayout, "");
+                        AddCellToBody_StockReport(tableLayout, "");
+                        AddCellToBody_StockReport(tableLayout, "Grand Total : " + grandTotal.ToString("."));
+                    }
+
+                }
+            }
+            else
+            {
+                AddCellToBody_StockReport(tableLayout, "");
+                AddCellToBody_StockReport(tableLayout, "");
+                AddCellToBody_StockReport(tableLayout, "");
+                AddCellToBody_StockReport(tableLayout, "");
+                AddCellToBody_StockReport(tableLayout, "");
+                AddCellToBody_StockReport(tableLayout, "");
+                AddCellToBody_StockReport(tableLayout, "");
+            }
+
+
+
+            return tableLayout;
+        }
+        // Method to add single cell to the Header  
+        private static void AddCellToHeader_StockReport(PdfPTable tableLayout, string cellText)
+        {
+
+            tableLayout.AddCell(new PdfPCell(new Phrase(cellText, new iTextSharp.text.Font(iTextSharp.text.Font.FontFamily.HELVETICA, 8, 1, iTextSharp.text.BaseColor.YELLOW)))
+            {
+                HorizontalAlignment = Element.ALIGN_LEFT,
+                Padding = 5,
+                BackgroundColor = new iTextSharp.text.BaseColor(128, 0, 0)
+            });
+        }
+
+        // Method to add single cell to the body  
+        private static void AddCellToBody_StockReport(PdfPTable tableLayout, string cellText)
         {
             tableLayout.AddCell(new PdfPCell(new Phrase(cellText, new iTextSharp.text.Font(iTextSharp.text.Font.FontFamily.HELVETICA, 8, 1, iTextSharp.text.BaseColor.BLACK)))
             {
---
+++ HaroonPOSWeb/Models/StockReportModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HaroonPOSWeb.Models
+{
+    public class StockReportModel
+    {
+
+        public Nullable<System.DateTime> CreateDate { get; set; }
+
+        [Required(ErrorMessage = "Select Month")]
+        public Nullable<int> MonthId { get; set; }
+
+        [Required(ErrorMessage = "Select Year")]
+        public Nullable<int> YearId { get; set; }
+    }
+}
